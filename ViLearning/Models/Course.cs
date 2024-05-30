7ed582b03824979d9e39051ca0344d6fbf94b17c
Võ Văn Quang <quangvo5803@gmail.com>
2024-05-30 14:50:00 +0700

    Demo Course Lesson

---
+++ ViLearning/Models/Course.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ViLearning.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseID { get; set; }
+
+        [Required]
+        [Display(Name = "Tên khóa học")]
+        public string CourseName { get; set; }
+        [Display(Name = "Giá khóa học")]
+        public double Price { get; set; }
+        [Display(Name = "Mô tả khóa học")]
+        public string Description { get; set; }
+        [Display(Name = "Ảnh bìa khóa học")]
+        [ValidateNever]
+        public string CoverImgUrl { get; set; }
+
+
+        //Foreign key
+        public string SubjectId { get; set; }
+        public string TeacherID { get; set; }
+
+
+        //Relation
+        [ForeignKey("SubjectId")]
+        [ValidateNever]
+        public Subject Subject { get; set; }      
+        [ForeignKey("TeacherId")]
+        [ValidateNever]     
+        public ApplicationUser Teacher { get; set; }
+        
+        
+        //Hiện tại còn thiếu Lesson,Certificate,Feedback
+    }
+}
---
+++ ViLearning/Models/Lesson.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ViLearning.Models
+{
+    public class Lesson
+    {
+        [Key]
+        public int LessonId { get; set; }
+        [Required]
+        public string LessonName { get; set; }
+        public bool statusBoolean { get; set; }
+
+
+        //Foreign key
+        public string CourseId { get; set; }
+
+        //Relation
+        [ForeignKey("CourseId")]
+        [ValidateNever]
+        public Course Course { get; set; }
+        
+
+        //Hiện tại còn thiếu Content
+    }
+}
