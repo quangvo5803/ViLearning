d1a361d29495672083c2e1430ba60ff76bf175d9
Nguyen Van Danh <danhvannguyen000@gmail.com>
2024-06-04 09:50:58 +0700

    add Grade to Course and basic UI for Course

--- ViLearning/Areas/Teacher/Controllers/HomeController.cs
+++ ViLearning/Areas/Teacher/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using ViLearning.Models;
+using ViLearning.Services.Repository.IRepository;
 using ViLearning.Utility;
 
 namespace ViLearning.Areas.Teacher.Controllers
@@ -8,13 +10,20 @@ namespace ViLearning.Areas.Teacher.Controllers
     [Authorize(Roles = SD.Role_User_Teacher)]
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult Courses()
         {
-            return View();
+            List<Course> courses = _unitOfWork.Course.GetAll().ToList();
+
+            return View(courses);
         }
     }
 }
