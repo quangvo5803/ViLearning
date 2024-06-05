10bd7352d9517747c230a4f93d32b5c5e144fb41
Nguyen Van Danh <danhvannguyen000@gmail.com>
2024-06-05 10:21:54 +0700

    Full UI for Teacher Courses

---
+++ ViLearning/Areas/Teacher/Controllers/CoursesController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ViLearning.Data;
+using ViLearning.Models;
+using ViLearning.Services.Repository;
+using ViLearning.Services.Repository.IRepository;
+
+namespace ViLearning.Areas.Teacher.Controllers
+{
+    [Area("Teacher")]
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+        private readonly IUnitOfWork _unitOfWork;
+        public CoursesController(ApplicationDBContext context, IUnitOfWork unitOfWork)
+        {
+            _context = context;
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: Teacher/Courses
+        public async Task<IActionResult> Index()
+        {
+            List<Course> courses = _unitOfWork.Course.GetAll().ToList();
+
+            return View(courses);
+        }
+
+        // GET: Teacher/Courses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .Include(c => c.ApplicationUser)
+                .Include(c => c.Subject)
+                .FirstOrDefaultAsync(m => m.CourseId == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        // GET: Teacher/Courses/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id");
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Name");
+            return View();
+        }
+
+        // POST: Teacher/Courses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CourseId,CourseName,Price,Description,CoverImgUrl,SubjectId,UserId,Grade")] Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(course);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", course.UserId);
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Name", course.SubjectId);
+            return View(course);
+        }
+
+        // GET: Teacher/Courses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses.FindAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", course.UserId);
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Name", course.SubjectId);
+            return View(course);
+        }
+
+        // POST: Teacher/Courses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CourseId,CourseName,Price,Description,CoverImgUrl,SubjectId,UserId,Grade")] Course course)
+        {
+            if (id != course.CourseId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(course);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CourseExists(course.CourseId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", course.UserId);
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Name", course.SubjectId);
+            return View(course);
+        }
+
+        // GET: Teacher/Courses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .Include(c => c.ApplicationUser)
+                .Include(c => c.Subject)
+                .FirstOrDefaultAsync(m => m.CourseId == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        // POST: Teacher/Courses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+            if (course != null)
+            {
+                _context.Courses.Remove(course);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CourseExists(int id)
+        {
+            return _context.Courses.Any(e => e.CourseId == id);
+        }
+    }
+}
