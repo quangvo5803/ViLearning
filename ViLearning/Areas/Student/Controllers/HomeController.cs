8ea58423eb3bc51dd3ab8d366660d7f382c9bba3
Võ Văn Quang <quangvo5803@gmail.com>
2024-06-09 20:51:16 +0700

    UI: Submit Certificate UI

--- ViLearning/Areas/Student/Controllers/HomeController.cs
+++ ViLearning/Areas/Student/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using ViLearning.Models;
@@ -50,7 +51,7 @@ namespace ViLearning.Areas.Student.Controllers
             };
             return View(detailViewModel);
         }
-
+        
         public IActionResult CourseList()
         {
             List<ApplicationUser> teacherList = new List<ApplicationUser>();
@@ -69,6 +70,42 @@ namespace ViLearning.Areas.Student.Controllers
                 TeacherList = teacherList
             };
             return View(viewModel);
+        }
+
+        public IActionResult Search(string query) 
+        {
+            List<ApplicationUser> teacherList = new List<ApplicationUser>();
+            List<ApplicationUser> userList = _unitOfWork.ApplicationUser.GetAll().ToList();
+            foreach (ApplicationUser user in userList)
+            {
+                if (user.Role == "Teacher")
+                {
+                    teacherList.Add(user);
+                }
+            }
+            var viewModel = new LandingPageVM
+            {
+                Courses = _unitOfWork.Course.GetRange(c => c.CourseName.Contains(query)
+                                                            || c.Subject.Name.Contains(query)
+                                                            || c.Description.Contains(query)
+                                                            || c.ApplicationUser.FullName.Contains(query), includeProperties: "Subject,ApplicationUser"),
+                UserList = userList,
+                TeacherList = teacherList
+            };
+            return View(viewModel);
+        }
+
+        [Authorize]
+        public IActionResult Checkout(int CourseId)
+        { 
+            var course = _unitOfWork.Course.Get(c=>c.CourseId==CourseId);
+            var lessonOfCourse = _unitOfWork.Lesson.GetRange(c => c.Course.CourseId == CourseId, includeProperties: "Course");
+            var detailViewModel = new CourseDetailsVM
+            {
+                Course = course,
+                Lessons = lessonOfCourse
+            };
+            return View(detailViewModel);
         }
         public IActionResult Privacy()
         {
