055fbb07edb37ef6fe443dfeaaa23509bd8bbdeb
Nguyen Van Danh <52613735+chesterdang@users.noreply.github.com>
2024-06-24 21:02:39 +0700

    Teacher (#13)
    * Test first commit -b Teacher
    
    * test
    
    * add Grade to Course and basic UI for Course
    
    * update Teacher Courses display UI
    
    * fixing commit
    
    * Full UI for Teacher Courses
    
    * Add CRUD for Lessons
    
    * AddCourseControllerAndUICourseLesson
    
    * Complete backend handler for Lessons
    
    * AddControllerUICourseLesson
    
    * feat:CRUD questions
    
    * Feat: CRUD questions
    
    * CourseAndLessonManagement1.0
    
    * feat: sort question by difficulty and search question
    
    * Fix connection between LessonDetail and QuestionListManagement
    
    * Pull Master Origin
    
    * Fix Conflict Merge
    
    * fix validate for lesson creating form
    
    * temp commit
    
    * Add Filter Course
    
    * Add Filter Course Controller
    
    * Feat Approve Reject Course
    
    * update stream video for create lesson controller
    
    * Revert "update stream video for create lesson controller"
    
    This reverts commit 6c53b91c799a2ea5318f1b3e0e7417a1491279ec.
    
    * update stream video for lesson controller
    
    * fix conflict
    
    ---------
    
    Co-authored-by: longtpit2573 <144187960+longtpit2573@users.noreply.github.com>
    Co-authored-by: Quydnp <quyduong0304@gmail.com>
    Co-authored-by: Võ Văn Quang <144187554+quangvo5803@users.noreply.github.com>

--- ViLearning/Areas/Teacher/Controllers/QuestionController.cs
+++ ViLearning/Areas/Teacher/Controllers/QuestionController.cs
@@ -137,8 +137,6 @@ namespace ViLearning.Areas.Teacher.Controllers
         [HttpPost]
         public async Task<IActionResult> Add(QuestionManageVM vm)
         {
-            if (vm.Question.QuestionType == QuestionType.Essay) 
-                vm.Question.RightAnswer = "";
             _unitOfWork.Question.Add(vm.Question);
             _unitOfWork.Save();
             int lessonId = vm.Question.LessonId;
@@ -149,8 +147,6 @@ namespace ViLearning.Areas.Teacher.Controllers
         [HttpPost]
         public async Task<IActionResult> Update(QuestionManageVM vm)
         {
-            if (vm.Question.QuestionType == QuestionType.Essay) 
-                vm.Question.RightAnswer = "";
             _unitOfWork.Question.Update(vm.Question);
             _unitOfWork.Save();
             int lessonId = vm.Question.LessonId;
